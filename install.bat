03251547870ff68eecd6d1147408b6ffaa82410b
Ronald Tse <ronald.tse@ribose.com>
2018-12-03 22:40:30 +0800

    Update repo names in install scripts and documentation

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ SET INSTALL_LOG_FILE=metanorma_install.log
 ECHO Installation start %DATE% %TIME% >> %INSTALL_LOG_FILE% 2>&1
 
 bitsadmin /transfer get ^
-	https://raw.githubusercontent.com/CAMOBAP795/metanorma-windows-setup/master/packages.config ^
+	https://raw.githubusercontent.com/riboseinc/metanorma-windows-setup/master/packages.config ^
 	%CD%\packages.config >> %INSTALL_LOG_FILE% 2>&1
 
 WHERE choco >> %INSTALL_LOG_FILE% 2>&1
