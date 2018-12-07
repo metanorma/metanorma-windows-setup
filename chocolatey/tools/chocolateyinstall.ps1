5eb8ef35986f6cae02802b6d2e679d6b5d097072
Alex B <abobrikovich@gmail.com>
2018-12-07 13:25:48 -0800

    Add metanorma chocolatey package

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿New-Item -ItemType file $Env:ChocolateyInstall\\bin\\xml2-config.bat
+New-Item -ItemType file $Env:ChocolateyInstall\\bin\\xslt-config.bat
+
+New-Variable -Name XsltDist -Value $Env:ChocolateyInstall\\lib\\xsltproc\\dist
+New-Variable -Name XsltInclude -Value $XsltDist\\include
+New-Variable -Name XsltLib -Value $XsltDist\\lib
+
+New-Variable -Name RubyBin -Value c:\\tools\\ruby25\\bin
+
+$Env:RubyBin\\gem install bundler metanorma-cli -- `
+  --with-xml2-include=$Env:XsltInclude\\libxml2 `
+  --with-xslt-include=$Env:XsltInclude `
+  --with-xml2-lib=$Env:XsltLib `
+  --with-xslt-lib=$Env:XsltLib
+SETX /M RUBY_DLL_PATH "$Env:ChocolateyInstall\\lib\\xsltproc\\dist\\bin;%RUBY_DLL_PATH%"
+refreshenv
+
+# Copy with removing version from filename (need because xslt_lib.so expect such names)
+Copy-Item $Env:ChocolateyInstall\\lib\\xsltproc\\dist\\bin\libxml2*.dll $Env:ChocolateyInstall\\lib\\xsltproc\\dist\\bin\\libxml2.dll -force
+Copy-Item $Env:ChocolateyInstall\\lib\\xsltproc\\dist\bin\libxslt*.dll $Env:ChocolateyInstall\lib\xsltproc\dist\bin\libxslt.dll* >> %INSTALL_LOG_FILE% 2>&1
+XCOPY /Y $Env:ChocolateyInstall\lib\xsltproc\dist\bin\libexslt*.dll $Env:ChocolateyInstall\lib\xsltproc\dist\bin\libexslt.dll* >> %INSTALL_LOG_FILE% 2>&1
+
+ECHO List applications... >> %INSTALL_LOG_FILE% 2>&1
+WHERE ruby gem bundle java docker >> %INSTALL_LOG_FILE% 2>&1
---
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿New-Variable -Name RubyBin -Value c:\\tools\\ruby25\\bin
+
+$Env:RubyBin\\gem uninstall metanorma-cli
+$Env:RubyBin\\clean
